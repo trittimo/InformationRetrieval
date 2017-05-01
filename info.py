8a197ce2874eb3ffaff7f4929407914c790e66c4
Michael Trittin <trittimo@rose-hulman.edu>
2017-05-01 16:24:11 -0400

    Ignore data files

--- info.py
+++ info.py
@@ -1,8 +1,21 @@
 #!python3
 import sys
+from html.parser import HTMLParser
+
+class InfoParser(HTMLParser):
+  def handle_starttag(self, tag, attrs):
+    print("Start tag: ", tag)
+
+  def handle_endtag(self, tag):
+    print("End tag: ", tag)
+
+  def handle_data(self, tag):
+    print("Data tag: ", tag)
 
 def main():
-  print("Hello")
+  parser = InfoParser()
+  parser.feed('<html><head><title>Test</title></head>'
+            '<body><h1>Parse me!</h1></body></html>')
   # TODO
 
 
