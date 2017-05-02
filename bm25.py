8683397971c09487e1f0f2e3a8b6b584b6bf070c
Andrew McKee <mckeeaj@rose-hulman.edu>
2017-05-02 14:20:43 -0400

    Added in module for Okapi BM25

--- bm25.py
+++ bm25.py
@@ -1,23 +1,31 @@
+#! C:\Python27\python.exe
+
 # bm25.py
 # Implementation of Okapi BM25
 # Andrew McKee and Michael Trittin
 
+import math
 
 # constants
 k1 = 1.2
 b = 0.75
+IDF_FLOOR = 0.1
 
 def IDF(N, qcnt):
 	numer = (N - qcnt) + 0.5
 	denom = qcnt + 0.5
-	return math.log(numer / denom)
+	val = math.log(numer / denom)
+	if (val < IDF_FLOOR):
+		val = IDF_FLOOR
+	return val
 
-def searchDocument(doc, q):
-	# TODO
-	length = 0
-	count = 0
-	return length, count
-	
+def searchDocument(doc, qs):
+	count = [0] * len(qs)
+	for word in doc:
+		for i in range(0, len(qs)):
+			if (word == qs[i]):
+				count[i] = count[i] + 1
+	return count
 
 def bm25(docs, query):
 	qs = query.split();
@@ -25,37 +33,51 @@ def bm25(docs, query):
 	if (N == 0):
 		print "You must specify a query"
 		return
-	avg_length = 1 # average doc length: TODO
+	D = len(docs)
+	if (D == 0):
+		print "You must provide documents"
+		return
+	dlengths = [len(i) for i in docs]
+	avg_length = reduce(lambda x, y: x + y, dlengths) / D
+	print "Average document length:", avg_length
 	result = {}
-	for qi in qs:
-		scores = []
-		search = []
-		n = 0
-		for doc in docs:
-			info = searchDocument(doc, qi)
-			search.append(info)
-			if (info[1] > 0): # count > 0, inc n(qi)
-				n = n + 1
-		idf = IDF(N, n)
-		for i in range(0, len(search)):
-			length = search[i][0]
-			tfreq = search[i][1] / search[i][0]
+	print "Searching..."
+	counts = [searchDocument(d, qs) for d in docs]
+	#print "Counts:", counts
+	print "Processing..."
+	qn = [0] * N
+	for result in counts:
+		for x in range(0, N):
+			if (result[x] > 0): # count > 0, inc n(qi)
+				qn[x] = qn[x] + 1
+	#print "qn:", qn
+	idf = [IDF(D, qn_i) for qn_i in qn]
+	#print "idf:", idf
+	scores = []
+	for d in range(0, D):
+		length = dlengths[d]
+		count = counts[d]
+		score = 0
+		for i in range(0, N):
+			tfreq = float(count[i]) / length
 			numer = tfreq * (k1 + 1)
 			denom = (tfreq) + (k1 * (1 - b + (b * length / avg_length)))
-			scores.append(idf * numer / denom)
-		result[qi] = scores
-	# Print result for debugging
-	print "[DEBUG] Score Results: (Pre-Summation)"
-	for key, value in d.iteritems():
-		print key, ':', value
-	# Compute final summed scores for the documents
-	scores = []
-	for i in range(0, N):
-		score = 0
-		for qscore in result:
-			score += qscore[i]
+			score = score + (idf[i] * numer / denom)
+			#print "Score: D=", d, "Q=", qs[i], "==>", score
 		scores.append(score)
 	# Print what we are returning
-	print "[DEBUG] Final Scores:", scores
+	print "Final Scores:", scores
 	return scores
 
+'''
+docs = [
+	["a", "b", "c", "c"],
+	["a", "d", "d", "e"],
+	["a", "c", "d", "e"],
+	["b"],
+	["c"]
+]
+
+bm25(docs, "b")
+bm25(docs, "a b")
+'''
